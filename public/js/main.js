18116de9b282ec7700bf22257a7fffca1e5c821a
chase099 <chasecorbett@outlook.com>
2025-04-27 18:03:06 -0400

    Add updated search bar functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,39 @@
-console.log("Page loaded successfully! :)");
+import React from 'react';
+import ParticleBackground from 'react-particle-backgrounds';
+
+const App = () => {
+    const settings = {
+        canvas: {
+            canvasFillSpace: true,
+            width: 200,
+            height: 200,
+            useBouncyWalls: false
+        },
+        particle: {
+            particleCount: 50,
+            color: '#94ecbe',
+            minSize: 2,
+            maxSize: 5
+        },
+        velocity: {
+            directionAngle: 0,
+            directionAngleVariance: 360,
+            minSpeed: 1,
+            maxSpeed: 3
+        },
+        opacity: {
+            minOpacity: 0,
+            maxOpacity: 0.5,
+            opacityTransitionTime: 3000
+        }
+    };
+
+    return (
+        <div>
+            <ParticleBackground settings={settings} />
+            {/* Your other content */}
+        </div>
+    );
+};
+
+export default App;
